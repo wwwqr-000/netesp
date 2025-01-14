8780dea9f38431357bfb24e340dc79568c0cead0
wwwqr <83577933+wwwqr-000@users.noreply.github.com>
2025-01-14 21:37:48 +0100

    W.I.P main setup + W.I.P bus + added config + added screen + added ssd1306 lib

---
+++ netesp/__init__.py
@@ -0,0 +1,9 @@
+from netesp import config, screen, bus
+from time import sleep
+
+screen.drawTxt("Test", 0, 0)
+screen.refresh()
+
+while True:
+    print("marf")
+    sleep(0.2)
---
+++ netesp/config.py
@@ -0,0 +1,6 @@
+beepPin = 13
+lBtnPin = 12
+rBtnPin = 14
+enterBtnPin = 27
+screenSCL = 26
+screenSDA = 25
