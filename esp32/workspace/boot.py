0de05469dc538d1e0b65d840aee5b05740ed0e1d
wwwqr <83577933+wwwqr-000@users.noreply.github.com>
2025-01-24 13:58:44 +0100

    Added editMode check for E32MPS-Tool

--- esp32/workspace/boot.py
+++ esp32/workspace/boot.py
@@ -1,2 +1,9 @@
-#import netesp
-print("Tmp build")
+from machine import I2C, Pin
+
+f = open("netesp/config.py", "r")
+
+exec(f.read())
+
+editModeBtn = Pin(enterBtnPin, Pin.IN, Pin.PULL_UP)
+
+if (editModeBtn.value() == 1): import netesp
